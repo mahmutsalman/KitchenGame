93ecf5e06728783aeec4121c6a0f0a012a93dc01
Mahmut Salman <csmahmutsalman@gmail.com>
2026-04-14 16:15:53 +0300

    test: add temp testing code to verify kitchen object parent transfer
    ## What Changed
    - ClearCounter.cs: Added secondClearCounter and testing serialized fields
    - ClearCounter.cs: Update() reassigns kitchenObject's parent counter to secondClearCounter on T key and logs the result
    
    ## Why
    - Temporary test to confirm SetClearCounter() correctly updates the item's parent reference
    - Simulates the item transfer that will later be driven by the player pickup system
    - Will be removed once proper pickup/placement logic is implemented

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,8 +4,19 @@ public class ClearCounter : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private ClearCounter secondClearCounter;
+    [SerializeField] private bool testing;
 
     private KitchenObject kitchenObject;
+
+    private void Update() {
+        if (testing && Input.GetKeyDown(KeyCode.T)) {
+            if (kitchenObject != null) {
+                kitchenObject.SetClearCounter(secondClearCounter);
+                Debug.Log(kitchenObject.GetClearCounter());
+            }
+        }
+    }
 
     public void Interact() {
         if (kitchenObject == null) {
