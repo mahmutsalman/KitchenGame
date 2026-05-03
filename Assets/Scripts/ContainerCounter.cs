821ab723a1e210f2021b4a982aad365c2942ef0f
Mahmut Salman <csmahmutsalman@gmail.com>
2026-05-03 16:19:42 +0300

    feat: add ContainerCounter script
    Inherits BaseCounter instead of MonoBehaviour + IKitchenObjectParent,
    so all 5 interface methods and counterTopPoint/kitchenObject fields
    come from the base class. Only Interact() is overridden.

---
+++ Assets/Scripts/ContainerCounter.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class ContainerCounter : BaseCounter {
+
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+
+    public override void Interact(Player player) {
+        if (!HasKitchenObject()) {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, GetKitchenObjectFollowTransform());
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
+        } else {
+            GetKitchenObject().SetKitchenObjectParent(player);
+        }
+    }
+}
